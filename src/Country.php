23f3a606f3f3058d77353b3bb279018e00eaeeba
Radis Ionut <ionut.radis@gmail.com>
2020-02-28 12:53:14 +0200

    initial commit

---
+++ src/Country.php
@@ -0,0 +1,21 @@
+<?php
+
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+		require('src/helpers.php');
+	
+	$key = 'Ro';
+	
+	getFlag($key, $countries);
+
+	function getFlag($key, $countries) {
+		echo $countries[strtoupper($key)];
+	}
+	
+}
+?>
